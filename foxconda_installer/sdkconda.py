227765fa035babca0f124b4899dd06e3b4d56df2
Fuehner Tim <fuehner@iisb.local>
2016-05-04 13:55:00 +0200

    - meta-package creation now done manually - more flexible templates

--- foxconda_installer/sdkconda.py
+++ foxconda_installer/sdkconda.py
@@ -11,25 +11,52 @@ import sys
 import glob
 import json
 
+metatmpl = '''\
+package:
+    name: %(_MP)s
+    version: %(_MP_VERSION)s
+
+requirements:
+    run:
+%(_packages)s
+
+about:
+    home: http://www.foxbms.org
+    license: BSD
+'''
+
 
 class SDKFoxConda(object):
-    _CONDA = 'conda'
-    _BUILD_ENV = 'FCBUILD'
-    _MP = 'foxcondadeps'
 
     def __init__(self, conda = None, build_env = None, mp = None):
+        self._CONDA = 'conda'
+        self._BUILD_ENV = 'FCBUILD'
+        self._MP = 'foxcondadeps'
+
         if conda:
             self._CONDA = conda
         if build_env:
             self._BUILD_ENV = build_env
         if mp:
             self._MP = mp
+        self._MP_VERSION = '0.1'
 
-    def createMP(self, deps = [], version = '0.1'):
-        cmd = self._CONDA + ' metapackage ' + self._MP + ' ' + version + ' --dependencies'
+    def createMP(self, deps = []):
+        self._packages = ''
         for _d in deps:
             if not _d.startswith(self._MP):
-                cmd += ' ' + _d 
+                self._packages += '        - %s\n' % _d
+        try:
+            os.makedirs('meta-recipe')
+        except Exception, e:
+            logging.warning(str(e))
+            pass
+        logging.debug(str(self.__dict__))
+
+        with open(os.path.join('meta-recipe', 'meta.yaml'), 'w') as f:
+            f.write(metatmpl % self.__dict__)
+
+        cmd = self._CONDA + ' build meta-recipe'
         logging.debug(cmd)
         status = subprocess.call(cmd, shell=True)
 
